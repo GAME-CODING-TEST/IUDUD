beb3818fc27020a753738c8dc7ae5f362e0d10a4
신현기 <lement3864@gmail.com>
2024-08-17 20:03:23 +0900

    FEAT: baekjoon 20438 added

--- hyun/21438_dailyCheck/21438_dailyCheck.cpp
+++ hyun/21438_dailyCheck/21438_dailyCheck.cpp
@@ -5,37 +5,54 @@ using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
+
 	cin.tie(NULL); cout.tie(NULL);
 	
 	int N, K, Q, M;
+
 	cin >> N >> K >> Q >> M;
 	
-	int arr[5003] = {0,};
+	int arr[5003] = {0, };
 	bool sleep[5003] = {false,};
-	for(int i=0;i<K;i++){
-		int a; cin >> a;
-		sleep[a] = true;
+
+	for (int i = 0; i < K; i++)
+  {
+		int a; 
+    cin >> a;
+		
+    sleep[a] = true;
 	}
 	
-	for(int i=0;i<Q;i++){
-		int a; cin >> a;
+	for (int i = 0; i < Q; i++)
+  {
+		int a; 
+    cin >> a;
 		int k = 1;
-		if(!sleep[a]){
-			while(a * k <= N+2){
+
+		if (!sleep[a])
+    {
+			while (a * k <= N + 2)
+      {
 				if(!sleep[a * k]) arr[a * k] = 1;
-				k++;
+				
+        k++;
 			}
 		}
 	}
 
-	int sum[5003] = {0,};
-	for(int i=3;i<=N+2;i++){
+	int sum[5003] = { 0, };
+
+	for (int i = 3; i <= N + 2; i++)
+  {
 		sum[i] = sum[i-1] + arr[i];
 	}
 	
-	for(int i=0;i<M;i++){
-		int a, b; cin >> a >> b;
-		cout << (b-a+1) - (sum[b] - sum[a-1]) << '\n';
+	for (int i = 0; i < M; i++)
+  {
+		int a, b; 
+    cin >> a >> b;
+		
+    cout << (b - a + 1) - (sum[b] - sum[a - 1]) << '\n';
 	}
 	return 0;
 }
