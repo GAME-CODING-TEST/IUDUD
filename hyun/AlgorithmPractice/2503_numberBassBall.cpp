b37bd2dc1a7fac2944a38c70c484beaaebdfc2ce
신현기 <lement3864@gmail.com>
2024-09-29 22:29:27 +0900

    FEAT: baekjoon

--- hyun/AlgorithmPractice/2503_numberBassBall.cpp
+++ hyun/AlgorithmPractice/2503_numberBassBall.cpp
@@ -2,9 +2,34 @@
 
 using namespace std;
 
+int n;
+int hint[102], strike[102], ball[102], result;
+
 int main() {
 
+	cin >> n;
 
+	for (int i = 0; i < n; ++i)
+		cin >> hint[i] >> strike[i] >> ball[i];
+
+	for (int i = 123; i <= 987; ++i) {
+		bool check = true;
+		  
+		if (checkNum(i))
+			continue;
+
+		for (int j = 0; j < n; ++j) {
+			if (!check)
+				break;
+
+			check = checkGame(j, i);
+		}
+
+		if (check)
+			result++;
+	}
+
+	cout << result;
 
 	return 0;
 }
